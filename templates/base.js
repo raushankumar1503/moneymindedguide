b9ee51bad821ab21a167184d185061cc3a7cc50b
Raushan Kumar <hello@moneymindedguide.com>
2026-09-09 15:22:26 +0530

    Add real favicon/icon files for Google search results (replace inline data URI)

--- templates/base.js
+++ templates/base.js
@@ -28,7 +28,11 @@ function head(site, page, jsonldBlocks) {
   <meta property="og:url" content="${canonical}">
   <meta property="og:site_name" content="${esc(site.siteName)}">
   <meta name="twitter:card" content="summary">
-  <link rel="icon" href="${site.favicon}">
+  <link rel="icon" href="/favicon.ico" sizes="any">
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="${site.fontsUrl}" rel="stylesheet">
